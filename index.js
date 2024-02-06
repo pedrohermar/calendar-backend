7ea85da441e86d35dc7a3b87b2b268cf11fbe6e8
Pedro <pedrohm92@gmail.com>
2024-02-06 11:12:44 +0100

    Creación y conexión de la DB

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const dbConnection = require('./database/config');
 require('dotenv').config();
 
 // Creación de servidor de express
-const app = express()
+const app = express();
+
+// Base de Datos
+dbConnection();
 
 // Directorio Público
-app.use( express.static('public') )
+app.use( express.static('public') );
 
 // Lectura y parseo del body
-app.use( express.json() )
+app.use( express.json() );
 
 // Rutas
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth'));
 
 // Escuchar peticiones
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
-})
+});
