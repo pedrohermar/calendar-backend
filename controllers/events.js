476bfcb661988d772607bab8d8c091462552640b
Pedro <pedrohm92@gmail.com>
2024-02-12 18:20:29 +0100

    getEvents configurado para devolver los eventos con id y nombre del usuario

--- controllers/events.js
+++ controllers/events.js
@@ -2,11 +2,13 @@ const { response } = require('express');
 const Event = require('../models/Event');
 
 
-const getEvents = (req, res = response) => {
+const getEvents = async(req, res = response) => {
+
+    const events = await Event.find().populate('user', 'name');
 
     res.json({
         ok: true,
-        msg: 'getEvents'
+        events
     })
 
 };
