8cc5f1e7d153aeb422b84f442739f4d5767112a1
Pedro <pedrohm92@gmail.com>
2024-02-12 18:13:04 +0100

    Creado modelo para eventos y configurada creación y validación de nuevos eventos

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require('../models/Event');
 
 
 const getEvents = (req, res = response) => {
@@ -10,12 +11,28 @@ const getEvents = (req, res = response) => {
 
 };
 
-const createEvent = (req, res = response) => {
+const createEvent = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'createEvent'
-    })
+    const event = new Event( req.body );
+
+    try {
+
+        event.user = req.uid;
+
+        await event.save();
+
+        res.json({
+            ok: true,
+            event
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 
 };
 
--- routes/events.js
+++ routes/events.js
@@ -5,20 +5,41 @@
 
 const { Router } = require('express');
 const router = Router();
+const { check } = require('express-validator')
 
+const { validateFields } = require('../middlewares/validateFields')
 const { validateJWT } = require('../middlewares/validateJWT');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+
+// Validación de JWT para todas
+router.use( validateJWT )
 
 // // Obtener eventos
-router.get('/', validateJWT, getEvents)
+router.get('/', getEvents)
 
 // // Crear un nuevo evento
-router.post('/', validateJWT, createEvent)
+router.post(
+    '/',
+    [
+        check('title','El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+        validateFields
+    ],
+    createEvent
+)
 
 // // Actualizar evento
-router.put('/:id', validateJWT, updateEvent)
+router.put(
+    '/:id',
+    updateEvent
+)
 
 // // Borrar evento
-router.delete('/:id', validateJWT, deleteEvent)
+router.delete(
+    '/:id',
+    deleteEvent
+)
 
 module.exports = router;
