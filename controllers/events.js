f9c6f2f65071cda334f87ec6717f85ec2bdbb1bc
Pedro <pedrohm92@gmail.com>
2024-02-13 10:33:53 +0100

    Configurado endpoint para actualizar eventos

--- controllers/events.js
+++ controllers/events.js
@@ -38,12 +38,48 @@ const createEvent = async (req, res = response) => {
 
 };
 
-const updateEvent = (req, res = response) => {
+const updateEvent = async(req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    })
+    const eventId = req.params.id;
+    const uid = req.uid
+
+    try {
+
+        const event = await Event.findById( eventId )
+
+        if( !event ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe'
+            })
+        }
+
+        if( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene permisos para editar este evento'
+            })
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventUpdated = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } ); // new true para que la variable devuelva el valor actualizado, y no el anterior.
+
+        res.json({
+            ok: true,
+            event: eventUpdated
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        });
+    };
 
 };
 
